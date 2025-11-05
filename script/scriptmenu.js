074c587bc507e0db1308e0f759568a97c2b19d1d
Kshirou-Infmg <plrg@aluno.ifnmg.edu.br>
2025-11-05 16:47:14 -0300

    scriptmenu.js

--- script/scriptmenu.js
+++ script/scriptmenu.js
@@ -1,3 +1,9 @@
     function toggleMenu() {
       document.getElementById("sidebar").classList.toggle("active");
     }
+    function openTab(tabId) {
+  const tabs = document.querySelectorAll('.tab-content');
+  tabs.forEach(tab => tab.style.display = 'none');
+
+  document.getElementById(tabId).style.display = 'block';
+}
