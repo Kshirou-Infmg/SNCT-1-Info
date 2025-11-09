a5cf5a9f6ef342c39ed971b47b5de91d9e4deead
Kshirou-Infmg <plrg@aluno.ifnmg.edu.br>
2025-11-08 21:06:23 -0300

    Update scriptmenu.js

--- script/scriptmenu.js
+++ script/scriptmenu.js
@@ -1,27 +1,15 @@
-    function toggleMenu() {
-      document.getElementById("sidebar").classList.toggle("active");
-    }
-    function openTab(tabId) {
+function toggleMenu() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
+function openTab(tabId) {
   const tabs = document.querySelectorAll('.tab-content');
   tabs.forEach(tab => tab.style.display = 'none');
-
   document.getElementById(tabId).style.display = 'block';
 }
 
 function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
-
-  if (document.body.classList.contains("dark-mode")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
+  const isDark = document.body.classList.toggle("dark-mode");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark-mode");
-  }
-});
-
 
